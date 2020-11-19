d22daaf3094cee5658e943ba217c06dc015938b8
Kumaravel Raman <6508077-KumaravelRaman@users.noreply.gitlab.com>
2020-11-19 15:40:22 +0530

    sms read

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,17 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 // import { SmsRetriever } from '@ionic-native/sms-retriever/ngx';
 import { AndroidPermissions } from '@ionic-native/android-permissions/ngx';
+import { HttpClientModule } from '@angular/common/http';
 import { SMS } from '@ionic-native/sms/ngx';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    FormsModule,
     IonicModule.forRoot(),
     AppRoutingModule
   ],
--- src/app/pages/sms/sms.page.ts
+++ src/app/pages/sms/sms.page.ts
@@ -1,3 +1,5 @@
+import { ToastService } from './../../services/toast/toast.service';
+import { AlertFnService } from './../../services/alert/alert-fn.service';
 import { Component, OnInit } from '@angular/core';
 declare var SMSReceive: any;
 
@@ -8,12 +10,16 @@ declare var SMSReceive: any;
 })
 export class SmsPage implements OnInit {
 
-  // sendMsg: any;
-  mobNo;
-  txt;
-  constructor() { }
+  mobNo: string = '';
+  txt = '';
+  orgMobNo;
+  constructor(
+    private alert: AlertFnService,
+    private toastfn: ToastService
+  ) { }
 
   ngOnInit() {
+    this.start();
   }
 
   start() {
@@ -22,14 +28,31 @@ export class SmsPage implements OnInit {
         console.log('watch started');
         document.addEventListener('onSMSArrive', (e: any) => {
           console.log('onSMSArrive()');
-          console.log(e);
-          this.mobNo = e.data.address;
-          this.txt = e.data.body;
-          console.log(JSON.stringify(e.data));
+          // console.log(e);
+          // this.mobNo = e.data.address;
+          // this.txt = e.data.body;
+          // console.log(this.mobNo);
+          // console.log(this.txt);
+          // alert(this.mobNo);
+          // alert(this.txt);
+          // this.assignValue(e.data);
+
+          const sms: any = e.data;
+
+          this.toastfn.toastFn(`You have received new sms from ${sms.address}`);
+          this.alert.msgAlertFn(`Mobile : ${sms.address}, content : ${sms.body}`);
+
+          // console.log(sms);
+          this.orgMobNo = this.getMobileNo(sms.address);
+          this.txt = sms.body;
+          // console.log(this.mobNo);
+          // console.log(JSON.stringify(e.data, null, '\t'));
+          // this.stop();
         });
       },
       () => { console.log('watch start failed'); }
     );
+    console.log(this.mobNo);
   }
 
   stop() {
@@ -37,6 +60,17 @@ export class SmsPage implements OnInit {
       () => { console.log('watch stopped'); },
       () => { console.log('watch stop failed'); }
     );
+  }
+
+
+  getMobileNo(num) {
+    if (num.includes('+91')) {
+      // console.log('yes', num);
+      const mechanicNumber = num.substring(3);
+      return mechanicNumber;
+      // this.orgMobNo = mechanicNumber;
+      // console.log(mechanicNumber);
+    }
   }
 
 }
