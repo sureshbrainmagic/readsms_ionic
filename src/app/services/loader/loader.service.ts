4246e272f82f71247c93d9a6dfd2f5fbd47e38fa
Kumaravel Raman <6508077-KumaravelRaman@users.noreply.gitlab.com>
2020-11-30 10:30:50 +0530

    loader finetune

--- src/app/services/loader/loader.service.ts
+++ src/app/services/loader/loader.service.ts
@@ -14,11 +14,11 @@ export class LoaderService {
   async present(msg) {
     this.isLoading = true;
     return await this.loadingCtrl.create({
-      spinner: null,
-      message : `bubbles`,
+      spinner: 'bubbles',
+      message : `Send SMS to Server ...`,
       // message: msg,
       //  message : `<img src="assets/loading.gif" class="img-align" /> <br/> <div class='ion-text-center'>${msg}</div> `,
-      // spinner: spin,  // "bubbles" | "circles" | "circular" | "crescent" | "dots" | "lines" | "lines-small" | null | undefined
+      // spinner: "bubbles",  // "bubbles" | "circles" | "circular" | "crescent" | "dots" | "lines" | "lines-small" | null | undefined
       // cssClass: 'my-custom-class',   // Write CSS in global.css
       // mode : 'ios',
       // duration: 20000,
